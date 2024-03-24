c26a79680ed1f29d40dcd1636c9beb6618fa2e0e
RanggaPtr <rangga2000putera@gmail.com>
2024-03-24 12:58:23 +0700

    P5_23_1C_PERCOBAAN3_PERTANYAAN3

--- P5/sumArray/mainSum23.java
+++ P5/sumArray/mainSum23.java
@@ -9,27 +9,41 @@ public class mainSum23 {
 
         System.out.println("==================================================");
         System.out.println("program menghitung keuntungan total (Satuan juataan : 6.9)");
+        System.out.print("Masukkan Jumlah Perusahaan : ");
+        int perusahaan = sc23.nextInt();
         System.out.print("Masukkan jumlah bulan :");
         int elm = sc23.nextInt();
-        sum23 sm23 = new sum23(elm);
+        sum23 sm23 = new sum23(perusahaan, elm);
         System.out.println("===================================================");
-        for (int i = 0; i < elm; i++) {
-            System.out.print("Masukkan untung bulan ke - " + (i + 1) + " = ");
-            sm23.keuntungan[i] = sc23.nextDouble();
+        for (int i = 0; i < perusahaan; i++) {
+            System.out.println("Perusahaan " + (i + 1));
+            for (int j = 0; j < elm; j++) {
+                System.out.print("Masukkan untung bulan ke - " + (j + 1) + " = ");
+                sm23.keuntungan[i][j] = sc23.nextDouble();
+            }
         }
 
+        sm23.hitungTotalPerusahaan();
         System.out.println("===================================================");
-        System.out.println("ALGORITMA - BRUTE FORCE");
-        System.out.println("Total keuntungan selama " + sm23.elemen +
-                " bulan adalah : " +
-                sm23.totalBF(sm23.keuntungan));
+        for (int i = 0; i < perusahaan; i++) {
+            System.out.println("Total keuntungan perusahaan " + (i + 1) + " adalah : " + sm23.totalPerusahaan[i]);
+        }
         System.out.println("===================================================");
-        System.out.println("ALGORITMA - DIVIDE & QONQUER");
-        System.out.println(
-                "Total keuntungan selama " +
-                        sm23.elemen +
-                        " bulan adalah : " +
-                        sm23.totalDC(sm23.keuntungan, 0, sm23.elemen - 1));
+        System.out.println("Total keuntungan semua perusahaan adalah : " + sm23.totalSemuaPerusahaan());
         System.out.println("===================================================");
+        
+        // System.out.println("===================================================");
+        // System.out.println("ALGORITMA - BRUTE FORCE");
+        // System.out.println("Total keuntungan selama " + sm23.elemen +
+        // " bulan adalah : " +
+        // sm23.totalBF(sm23.keuntungan));
+        // System.out.println("===================================================");
+        // System.out.println("ALGORITMA - DIVIDE & QONQUER");
+        // System.out.println(
+        // "Total keuntungan selama " +
+        // sm23.elemen +
+        // " bulan adalah : " +
+        // sm23.totalDC(sm23.keuntungan, 0, sm23.elemen - 1));
+        // System.out.println("===================================================");
     }
 }
--- P5/sumArray/sum23.java
+++ P5/sumArray/sum23.java
@@ -1,31 +1,52 @@
 package P5.sumArray;
 
 public class sum23 {
+    int perusahaan;
     int elemen;
-    double keuntungan[], total;
+    double keuntungan[][];
+    double totalPerusahaan[];
 
-    sum23(int elemen) {
+    sum23(int perusahaan, int elemen) {
+        this.perusahaan = perusahaan;
         this.elemen = elemen;
-        this.keuntungan = new double[elemen];
-        this.total = 0;
+        this.keuntungan = new double[perusahaan][elemen];
+        this.totalPerusahaan = new double[perusahaan];
+    }
+    
+    void hitungTotalPerusahaan() {
+        for (int i = 0; i < perusahaan; i++) {
+            double total = 0;
+            for (int j = 0; j < elemen; j++) {
+                total += keuntungan[i][j];
+            }
+            totalPerusahaan[i] = total;
+        }
     }
 
-    double totalBF(double arr[]) {
-        for (int i = 0; i < elemen; i++) {
-            total = total + arr[i];
+    double totalSemuaPerusahaan() {
+        double total = 0;
+        for (int i = 0; i < perusahaan; i++) {
+            total += totalPerusahaan[i];
         }
         return total;
     }
 
+    
     double totalDC(double arr[], int l, int r) {
         if (l == r) {
             return arr[l];
         } else if (l < r) {
             int mid = (l + r) / 2;
             double lsum = totalDC(arr, l, mid);
-            double rsum = totalDC(arr, mid +1, r);
+            double rsum = totalDC(arr, mid + 1, r);
             return lsum + rsum;
         }
         return 0;
     }
+    // double totalBF(double arr[]) {
+    //     for (int i = 0; i < elemen; i++) {
+    //         total = total + arr[i];
+    //     }
+    //     return total;
+    // }
 }
