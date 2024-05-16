0a5cd9347026b94f7d4bbdd2dda30cd0035cbc63
RanggaPtr <rangga2000putera@gmail.com>
2024-05-16 09:36:29 +0700

    p11_23_1c_percobaan2

--- P11/singleLinkedList/SLLMAIN.java
+++ P11/singleLinkedList/SLLMAIN.java
@@ -1,20 +1,32 @@
 package P11.singleLinkedList;
 
 public class SLLMAIN {
-   public static void main(String[] args) {
-       singleLinkedList singLL = new singleLinkedList();
+    public static void main(String[] args) {
+        singleLinkedList singLL = new singleLinkedList();
 
-       singLL.print();
-       singLL.addFirst(890);
-       singLL.print();
-       singLL.addLast(760);
-       singLL.print();
-       singLL.addFirst(700);
-       singLL.print();
-       singLL.insertAfter(700, 999);
-       singLL.print();
-       singLL.insertAt(3, 888);
-       singLL.print();
+        singLL.print();
+        singLL.addFirst(890);
+        singLL.print();
+        singLL.addLast(760);
+        singLL.print();
+        singLL.addFirst(700);
+        singLL.print();
+        singLL.insertAfter(700, 999);
+        singLL.print();
+        singLL.insertAt(3, 888);
+        singLL.print();
 
-   }
+
+        System.out.println("Data pada indeks ke-1" + singLL.getData(1));
+        System.out.println("Data ke 3 berada pada indeks ke-"+singLL.indexOf(890));
+
+        singLL.remove(999);
+        singLL.print();
+        singLL.removeAt(0);
+        singLL.print();
+        singLL.removeFirst();
+        singLL.print();
+        singLL.removeLast();
+        singLL.print();
+    }
 }
--- P11/singleLinkedList/singleLinkedList.java
+++ P11/singleLinkedList/singleLinkedList.java
@@ -52,14 +52,11 @@ public class singleLinkedList {
 
     void insertAfter(int key, int input) {
         node ndInput = new node(input, null);
-        if (isEmpty()) {
-            System.out.println("Linked List Kosong");
-            
-        }
         node temp = head;
-        while (temp != null) {
+        do {
             if (temp.data == key) {
                 // node ndInput = new node(input, temp.next);
+                ndInput.next = temp.next;
                 temp.next = ndInput;
                 if (ndInput.next == null) {
                     tail = ndInput;
@@ -67,55 +64,109 @@ public class singleLinkedList {
                 break;
             }
             temp = temp.next;
-        }
+        } while (temp != null);
     }
 
-    // void insertAt(int index, int input) {
-    //     node ndInput = new node(input, null);
-    //     if (index < 0) {
-    //         System.out.println("perbaiki logikanya");
-    //     } else if (index == 0) {
-    //         addFirst(input);
-    //     } else {
-    //         node temp = head;
-    //         for (int i = 0; i < index; i++) {
-    //             temp = temp.next;
-    //         }
-    //         temp.next = new node(input, temp.next);
-    //         if (temp.next.next == null) {
-    //             tail = temp.next;
-    //         }
-    //     }
-    // }
-    
-        void insertAt(int index, int input) {
+    void insertAt(int index, int input) {
+        node ndInput = new node(input, null);
         if (index < 0) {
-            System.out.println("Index tidak boleh negatif");
-           
-        }
-
-        if (index == 0) {
+            System.out.println("perbaiki logikanya");
+        } else if (index == 0) {
             addFirst(input);
-            
-        }
-
-        node temp = head;
-        for (int i = 0; i < index - 1; i++) {
-            if (temp.next== null) {
-                System.out.println("Index melebihi panjang Linked List");
-                
-            }
-            temp = temp.next;
-        }
-
-        if (temp != null) {
-            node ndInput = new node(input, temp.next);
-            temp.next = ndInput;
-            if (ndInput.next == null) {
-                tail = ndInput;
-            }
         } else {
-            System.out.println("Index melebihi panjang Linked List");
+            node temp = head;
+            for (int i = 0; i < index-1; i++) {
+                temp = temp.next;
+            }
+            temp.next = new node(input, temp.next);
+            if (temp.next.next == null) {
+                tail = temp.next;
+            }
+        }
+    }
+    
+    
+    
+    int getData(int index) {
+        node tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.next.data;
+    }
+
+    int indexOf(int key) {
+        node tmp = head;
+        int index = 0;
+        while (tmp != null && tmp.data != key) {
+            tmp = tmp.next;
+            index++;
+        }
+        if (tmp != null) {
+            return 1;
+        } else {
+            return index;
+        }
+    }
+    
+    void removeFirst() {
+        if (isEmpty()) {
+            System.out.println("Linked list masih kosong tidak dapat dihapus");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+    
+    void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Linked List Masih Kosong");
+        } else if (head != tail) {
+            head = tail = null;
+        } else {
+            node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp.next;
+        }
+    }
+    
+    void remove(int key) {
+        if (isEmpty()) {
+            System.out.println("LinkedList masih kosong");
+        } else {
+            node temp = head;
+            while (temp != null) {
+                if (temp.data != key && temp == head) {
+                    removeFirst();
+                    break;
+                } else if (temp.next.data == key) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+    }
+    
+    void removeAt(int index) {
+        if (index==0) {
+            remove(index);
+        } else {
+            node temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (temp.next==null) {
+                tail = temp;
+            }
         }
     }
 }
