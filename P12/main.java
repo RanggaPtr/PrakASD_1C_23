8cf2908e78a707730afde2d5e5016858127193c9
RanggaPtr <rangga2000putera@gmail.com>
2024-05-19 14:57:26 +0700

    P12_23_1C_PERCOBAAN2

--- P12/main.java
+++ P12/main.java
@@ -1,7 +1,7 @@
 package P12;
 
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         doubleLinkedList dll = new doubleLinkedList();
         dll.print();
         System.out.println("Size :" + dll.size());
@@ -19,9 +19,24 @@ public class main {
         dll.clear();
         dll.print();
         System.out.println("Size :"+dll.size());
-        
-
-
-
+        System.out.println("=========================");
+        dll.addLast(50);
+        dll.addLast(40);
+        dll.addLast(10);
+        dll.addLast(20);
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=========================");
+        dll.removeFirst();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=========================");
+        dll.removeLast();
+        dll.print();
+        System.out.println("Size : " + dll.size());
+        System.out.println("=========================");
+        dll.remove(1);
+        dll.print();
+        System.out.println("Size : " + dll.size());
     }
 }
