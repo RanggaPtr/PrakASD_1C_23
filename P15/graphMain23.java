16428ea40ad31a60ead0339a020e965da1e34e35
RanggaPtr <rangga2000putera@gmail.com>
2024-06-07 19:26:38 +0700

    P15_23_1C_RANGGA PUTRA SYANANDA BUDHI_LATIHAN_PRAKTIKUM_PERTANYAAN1

--- P15/graphMain23.java
+++ P15/graphMain23.java
@@ -1,21 +1,84 @@
 package P15;
-
+import java.util.*;
 public class graphMain23 {
     public static void main(String[] args) throws Exception {
-        graph23 g = new graph23(6);
-        g.addEdge(0, 1, 50);
-        g.addEdge(0, 2, 100);
-        g.addEdge(1, 3, 70);
-        g.addEdge(2, 3, 40);
-        g.addEdge(3, 4, 60);
-        g.addEdge(4, 5, 80);
-        g.degree(0);
-        g.printGraph();
-        g.removeEdge(1, 3);
-        g.printGraph();
+        // graph23 g = new graph23(6);
+        // g.addEdge(0, 1, 50);
+        // g.addEdge(0, 2, 100);
+        // g.addEdge(1, 3, 70);
+        // g.addEdge(2, 3, 40);
+        // g.addEdge(3, 4, 60);
+        // g.addEdge(4, 5, 80);
+        // g.degree(0);
+        // g.printGraph();
+        // g.removeEdge(1, 3);
+        // g.printGraph();
 
-        checkPath(g, 2, 3); // C to D
-        checkPath(g, 2, 5); // C to F
+        // checkPath(g, 2, 3); // C to D
+        // checkPath(g, 2, 5); // C to F
+        
+        Scanner sc = new Scanner(System.in);
+        graph23 g = new graph23(6);
+
+        int choice;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1. Add Edge");
+            System.out.println("2. Remove Edge");
+            System.out.println("3. Degree");
+            System.out.println("4. Print Graph");
+            System.out.println("5. Check Path");
+            System.out.println("6. Exit");
+            System.out.print("Choose an option: ");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter source (0-5): ");
+                    int source = sc.nextInt();
+                    System.out.print("Enter destination (0-5): ");
+                    int destination = sc.nextInt();
+                    System.out.print("Enter distance: ");
+                    int distance = sc.nextInt();
+                    g.addEdge(source, destination, distance);
+                    break;
+
+                case 2:
+                    System.out.print("Enter source (0-5): ");
+                    source = sc.nextInt();
+                    System.out.print("Enter destination (0-5): ");
+                    destination = sc.nextInt();
+                    g.removeEdge(source, destination);
+                    break;
+
+                case 3:
+                    System.out.print("Enter vertex (0-5): ");
+                    int vertex = sc.nextInt();
+                    g.degree(vertex);
+                    break;
+
+                case 4:
+                    g.printGraph();
+                    break;
+
+                case 5:
+                    System.out.print("Enter source (0-5): ");
+                    source = sc.nextInt();
+                    System.out.print("Enter destination (0-5): ");
+                    destination = sc.nextInt();
+                    checkPath(g, source, destination);
+                    break;
+
+                case 6:
+                    System.out.println("Exiting...");
+                    break;
+
+                default:
+                    System.out.println("Invalid option. Please try again.");
+            }
+        } while (choice != 6);
+
+        sc.close();
     }
     
     private static void checkPath(graph23 g, int asal, int tujuan) {
